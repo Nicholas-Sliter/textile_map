3825fe2572c4dd18fa13b10d252e5c14b016fc8a
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2021-02-06 19:51:55 -0500

    Update to functions.R and test.R (soon to be clean.R)

--- functions.R
+++ functions.R
@@ -8,9 +8,10 @@ new_deb_lsd_col <- function(data,
                             l="value_guldens",
                             s="value_stuivers",
                             d="value_penningen"){
-  return(data %>% mutate(deb_lsd = deb_lsd(getElement(data,l),
-                                           getElement(data,s),
-                                           getElement(data,d),
+  
+  return(data %>% mutate(deb_lsd = deb_lsd(ifelse(is.na(getElement(data,l)),0,as.numeric(getElement(data,l))),
+                                           ifelse(is.na(getElement(data,s)),0,as.numeric(getElement(data,s))),
+                                           ifelse(is.na(getElement(data,d)),0,as.numeric(getElement(data,d))),
                                            bases = c(20, 16))))
   
 }
@@ -20,9 +21,9 @@ new_deb_dec_col<- function(data,
                            s="value_stuivers",
                            d="value_penningen"){
   return(data %>% mutate(deb_dec = as.numeric(deb_as_decimal(deb_lsd(getElement(data,l),
-                                           getElement(data,s),
-                                           getElement(data,d),
-                                           bases = c(20, 16))))))
+                                                                     getElement(data,s),
+                                                                     getElement(data,d),
+                                                                     bases = c(20, 16))))))
   
 }
 
@@ -32,7 +33,58 @@ deb_deb_from_lsd<- function(data,colname = 'deb_lsd'){
   
 }
 
+#return the *type* (if numeric or not) of a vector
+#return TRUE if numeric, FALSE if not numeric or factored
+detect_varType <- function(data,colname){
+  vector <- getElement(data,colname)
+  if(is.factor(vector)){
+    return(FALSE)
+  }
+  else if(!is.numeric(vector)){
+    return(FALSE)
+  }
+  else if (is.numeric(vector)){
+    return(TRUE) 
+  }
+  else{
+    return(FALSE) 
+  }
+}
 
+
+get_graphType <- function(data,x,y,args=c(main=c(),aes=c())){
+  x_type <- detect_varType(data,x)
+  y_type <- detect_varType(data,y)
+  
+  
+  #X and Y are both cont.
+  if (x_type & y_type){
+    #scatterplot
+    return(
+      geom_point(args[1],aes(args[2]))
+    )
+    
+  }
+  #X XOR Y are cont.
+  else if(xor(x_type,y_type)){
+    #barchart
+    return(
+      geom_col()
+    )
+  }
+  #X and Y are discrete
+  else{
+    #use a bubble plot using geom_count
+    return(
+      geom_count(args[1],aes(args[2]))
+    )
+  }
+  
+  
+  #to do args, get length of vector
+  
+  
+}
 
 
 
--- test.R
+++ test.R
@@ -6,12 +6,13 @@ library(readxl)
 library(stringr)
 library(leaflet)
 library(debkeepr)
+#source to function file
+source('functions.R')
 
 
-
-VOC <- read_xlsx(file.choose())
+VOC <- read_xlsx('VOC_2021_TextilesDataset.xlsx')
 VOC_main <-  VOC
-WIC <- read_xlsx(file.choose())
+WIC <- read_xlsx('WIC_2021_Total_InELLS.xlsx')
 WIC_main <- WIC
 
 #clean here  
@@ -19,23 +20,40 @@ VOC_main$total_value <- str_remove_all(VOC_main$total_value,'\\.','')
 VOC_valueVector <- str_split(VOC_main$total_value,',')
 
 
-VOC_deb <- VOC_main %>% mutate(value_guldens = as.numeric(value_guldens),
-                              value_stuivers = as.numeric(value_stuivers),
-                              value_penningen = as.numeric(value_penningen)) 
+#add if NA then set to 0
+VOC_deb <- VOC_main %>% mutate(value_guldens = ifelse(is.na(value_guldens),
+                                                      0,
+                                                      as.numeric(value_guldens)),
+                               value_stuivers = ifelse(is.na(value_stuivers),
+                                                       0,
+                                                       as.numeric(value_stuivers)),
+                               value_penningen = ifelse(is.na(value_penningen),
+                                                        0,
+                                                        as.numeric(value_penningen))) 
+
+
+
+
 VOC_deb <- VOC_deb %>% mutate(deb_lsd = deb_lsd(l = VOC_deb$value_guldens,
                                                 s = VOC_deb$value_stuivers,
                                                 d = VOC_deb$value_penningen,
                                                 bases = c(20, 16)
-  
-  ))
+                                                
+))
+
+
+
+
+
+
 
 
 #if NA then set to 0
 
 new_deb_lsd_col <- function(data,
-                            l=value_guldens,
-                            s=value_stuivers,
-                            d=value_penningen){
+                            l="value_guldens",
+                            s="value_stuivers",
+                            d="value_penningen"){
   return(data %>% mutate(deb_lsd = deb_lsd(getElement(data,l),
                                            getElement(data,s),
                                            getElement(data,d),
@@ -44,128 +62,97 @@ new_deb_lsd_col <- function(data,
 }
 
 
+WIC_deb <- WIC_main %>% new_deb_lsd_col()
+WIC_deb <- WIC_deb %>% mutate(deb_dec = as.numeric(deb_as_decimal(WIC_deb$deb_lsd)))
+
 VOC_deb <- VOC_deb %>% new_deb_lsd_col()
+VOC_deb <- VOC_deb %>% mutate(deb_dec = as.numeric(deb_as_decimal(VOC_deb$deb_lsd)))
 
-%>%
-  mutate(textile_quantity = as.numeric(str_replace_all(
-                                        str_split(textile_quantity, " ")[1],
-                                        
-    textile_quantity)
+for (i in 1:length(VOC_deb$textile_quantity)){
+  #remove bad characters, round, change to numeric, etc, set to NA if bad
+  
+  VOC_deb$textile_quantity[i] <- str_split(VOC_deb$textile_quantity[i], " ")[[1]][1]
+  VOC_deb$textile_quantity[i] <- str_split(VOC_deb$textile_quantity[i], "\\.")[[1]][1]
+  VOC_deb$textile_quantity[i] <- ifelse(str_detect(VOC_deb$textile_quantity[i],"[:alpha:][:punct:]&!\\."),NA,VOC_deb$textile_quantity[i])
+  VOC_deb$textile_quantity[i] <- as.numeric(VOC_deb$textile_quantity[i])
+  
+}
 
-    
-  for (i in 1:length(VOC_deb$textile_quantity)){
-    #remove bad characters, round, change to numeric, etc, set to NA if bad
-    
-    VOC_deb$textile_quantity[i] <- str_split(VOC_deb$textile_quantity[i], " ")[[1]][1]
-    VOC_deb$textile_quantity[i] <- str_split(VOC_deb$textile_quantity[i], "\\.")[[1]][1]
-    VOC_deb$textile_quantity[i] <- ifelse(str_detect(VOC_deb$textile_quantity[i],"[:alpha:][:punct:]&!\\."),NA,VOC_deb$textile_quantity[i])
-    VOC_deb$textile_quantity[i] <- as.numeric(VOC_deb$textile_quantity[i])
-    
-  }
-  
-  
-joined <- left_join(WIC_
-  
 
-    
+#make price per unit and fill out quality_inferred 
+
+#get rid of empty 39th col then remove the uneccesary cols
+VOC_toJoin <- VOC_deb[-39]
+VOC_toJoin <- VOC_toJoin %>% select(-'source',
+                                    -'exchange_nr',
+                                    -'orig_date',
+                                    -'dest_date',
+                                    -'textile_subcategory',
+                                    -'textile_other_unknown_arch',
+                                    -'value_currency',
+                                    -'guilders_per',
+                                    -'stuivers_per',
+                                    -'pennigen_per',
+                                    -'meas_per',
+                                    -'value_guldens',
+                                    -"value_stuivers",
+                                    -"value_penningen",
+                                    -'deb_lsd')
+#Change types and add quant_ells and unit ells cols
+VOC_toJoin <- VOC_toJoin %>% 
+  mutate(quant_ells = as.numeric(NA), units_ells = as.numeric(NA)) %>%
+  mutate(dest_yr = as.numeric(dest_yr)) %>%
+  mutate(textile_quantity = as.numeric(textile_quantity))
+
+#change col names
+colnames(VOC_toJoin)[which(names(VOC_toJoin) == "dest_loc_port_modern")] <- "dest_loc_port"
+colnames(VOC_toJoin)[which(names(VOC_toJoin) == "dest_loc_region_modern")] <- "dest_loc_region"           
+
+WIC_toJoin <- WIC_deb %>% select(-'source',
+                                 -'exchange_nr',
+                                 -'orig_date',
+                                 -'dest_date',
+                                 -'textile_subcategory',
+                                 -'textile_other_unknown_arch',
+                                 -'value_currency',
+                                 -'guilders_per',
+                                 -'stuivers_per',
+                                 -'pennigen_per',
+                                 -'meas_per',
+                                 -'value_guldens',
+                                 -"value_stuivers",
+                                 -"value_penningen",
+                                 -'deb_lsd')
+#set dest yr to final dest yr if dest_yr is NA, then remove final_dest_yr, and change types
+WIC_toJoin <- WIC_toJoin %>% mutate(dest_yr = ifelse(is.na(dest_yr), final_dest_yr, dest_yr)) %>%
+  select(-'final_dest_yr') %>% mutate(units_ells = as.numeric(units_ells))
+
+
+joined <- full_join(WIC_toJoin,VOC_toJoin,by=colnames(WIC_toJoin))
+#use quant_ells if available if not, use textile_quantity
+
+
+VOC_toJoin <- VOC_toJoin %>%
+  mutate(ID = paste(company, row_number(), sep = "_"))
+WIC_toJoin <- WIC_toJoin %>%
+  mutate(ID = paste(company, row_number(), sep = "_"))
+
+
+
+joined <- full_join(VOC_toJoin, WIC_toJoin,
+                    by = "ID")
+
+#write all to files
+write.csv(joined,'joined.csv')
+write.csv(WIC_toJoin,'WIC_clean.csv')
+write.csv(VOC_toJoin,'VOC_clean.csv')
 # DOnt remove . , but round to nearest whole
 #replace x with 0, replace strings to NA, replace entire string with NA if / is present?
 #split weird fractions before fraction
 
-
-#create the correct currency base for the Dutch with debkeepr by mutating a new column
-lsd_Acredam <- WIC_Acredam %>% 
-  mutate(lsd = deb_lsd(l = value_guldens, s = value_stuivers, d = value_penningen, 
-                       bases = c(20,16)))
-
-#create a graphable decimal column with Dutch currency values
-deci_Acredam <- lsd_Acredam %>%
-  mutate(deci_lsd = deb_as_decimal(lsd))
-
-
-
-VOC.data <- VOC.data.original %>%
-  mutate(quantity.clean = ifelse(str_detect(textile_quantity,
-                                            "[:punct:]&!\\."),
-                                 NA,
-                                 as.numeric(textile_quantity)),
-         guldens.clean = ifelse(is.na(value_guldens),
-                                0,
-                                as.numeric(value_guldens)),
-         stuivers.clean = ifelse(is.na(value_stuivers),
-                                 0,
-                                 as.numeric(value_stuivers)),
-         pennigen.clean = ifelse(is.na(value_penningen),
-                                 0,
-                                 as.numeric(pennigen.clean)),
-         lsd = deb_lsd(l = guldens.clean, s = stuivers.clean, d = pennigen.clean, base = c(20, 16)),
-         dec = as.numeric(deb_as_decimal(lsd)))
-
-
-
-VOC.data <- VOC.original %>%
-  mutate(quantity.clean = ifelse(str_detect(textile_quantity,
-                                            "([:alpha:]|[:punct:])&!\\."),
-                                 NA,
-                                 round(as.numeric(textile_quantity)))
          
-
-
-
-
-Join <- 
-  
-
-
-  
-  
-#Get data, read it in, clean it, join it
-  
-#Shiny UI with inputs and outputs and layout
-  
-#Leaflet graphs + others? that use either VOC or WIC or both + the input selectors (for fabric type and modifiers?)
-  
-#We want: show a map that has all countries where textiles are shipped to, displays quantity and price
-#bubble graph (with quantity/and or price) click on bubble to get info section
-
-#summary/graph on bubble click?
-#
-  
-  
-  
-  
-  
-  
-  
-  
-
-
-
-
-
-
-
-
-#import both WIC and VOC Or *let user choose between WIC and VOC*
-
-VOC = read_xlsx()
-VOC_main <- 
-  WIC = read_xlsx()
-WIC_main <-
-  Join =
-  
-  
-  #let user choose VOC or WIC
-  choice <- input$data_set #WIC, VOC, or both
-data <- assign(input$data_set)
-
-#cleaning, dependent on data set chosen
-
-
-#mutate cols for lat and long based on user selection
-#use variables set to inputs to accomplish this
-
-
-
-
-leaflet() %>% addMarkers() %>% addTiles()
+         
+         
+         
+         
+         
